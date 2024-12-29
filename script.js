bb7f2f76b9f421ed4e63e270045e36a2f4d4104e
= <2022pcecrnikhil040@poornima.org>
2024-12-29 13:53:26 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Contact form submitted Successfully!');
+});
+
+document.getElementById('reachUsForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Inquiry Form Submitted Successfully!');
+});
